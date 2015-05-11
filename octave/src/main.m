525ff881e0a7aaa33e30994ae89776e1a66ef668
Lucas Simões <simoes.lucas.silva@gmail.com>
2015-05-11 14:56:20 -0300

    naiveStep completed

--- octave/src/main.m
+++ octave/src/main.m
@@ -1,12 +1,62 @@
 function [ind v] = simplex (A, b, c, m, n, x)
 endfunction
 
-function naiveStep(A, b_ind, x_B)
-    B = A(:, b_ind)
+function [ind v] = naiveStep(A, m, c, bind, xB)
+    B = A(:, bind)
     [L, U] = lu(B)
-    not_b = []
-    for i=1:size(A, )
-        not_b = [not_b, ]
+
+    [redcj, j] = reducedCost(A, c, bind, L, U)
+    if j == 0
+        ind = 0
+        v = xB
+        return
+    endif
+
+    y = L \ A(j)
+    dB = - (U \ y)
+
+    if dB >= 0
+        v = dB
+        ind = -1
+        return
+    endif
+
+    [theta, i] = thetaStep(xB, dB, m)
+
+    bind(i) = j
+    v = xB + theta*dB
+    ind = 1
+endfunction
+
+function [theta imin] = thetaStep(xB, dB, m)
+    theta = Inf
+    for i=1:m
+        if dB(i) < 0
+            aux = - xB(i) / dB(i)
+            if aux < theta
+                theta = aux
+                imin = i
+            endif
+        endif
+    endfor
+endfunction
+
+function [redcj ind] = reducedCost(A, c, bind, L, U)
+    cB = c(bind)
+    for j=1:m
+        if any(j == bind)
+            continue;
+        endif
+
+        y = U' \ cB
+        p = L' \ y
+
+        redcj = c(j) - p'*A(:,j)
+        if redcj < 0
+            ind = j
+            return
+        endif
     endfor
 
+    ind = 0
 endfunction
