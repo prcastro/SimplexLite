e8d9bb927c57bf172a44963cbc0bd1d3f9f00f03
brk00 <p.oliveira.castro@gmail.com>
2015-05-12 10:42:01 -0300

    Code cleanup

--- octave/src/naiveStep.m
+++ octave/src/naiveStep.m
@@ -1,21 +1,19 @@
-function [vec ind i j theta] = naiveStep(A, m, c, bind, nbind, xB)
+function [v ind i j] = naiveStep(A, m, n, c, bind, nbind, v)
+    # Compute B and its LU decomposition
     B = A(:, bind);
     [L, U] = lu(B);
 
     # Compute the reduced costs
-    #
     [redc, aux] = reducedCost(A, c, bind, nbind, L, U);
-
     printf("Custos Reduzidos\n");
     print_vec(nbind, redc);
 
     # When aux = 0, reduced costs are all non-negative
     # and we found an optimal solution
     if aux == 0
-        vec = xB;
         ind = 0;
-        i = 0
-        j = 0
+        i = 0;
+        j = 0;
         return;
     endif
 
@@ -23,32 +21,37 @@ function [vec ind i j theta] = naiveStep(A, m, c, bind, nbind, xB)
     j = nbind(aux);
 
     # Find j-th basic direction
-    dB = - (U \ (L \ A(:, j)));
+    d       = zeros(n, 1);
+    d(bind) = - (U \ (L \ A(:, j)));
+    d(j)    = 1;
 
     # If non-negative, this direction leads to cost = -Inf
-    if dB >= 0
-        vec = dB;
+    if d >= 0
+        # We return v as the direction that leads to cost = -Inf
+        v = d;
         ind = -1;
-        i = 0
-        j = 0
+        i = 0;
+        j = 0;
         return;
     endif
 
+    # Index j is the one that enters the base
     printf("\nEntra na base: %d\n", j);
+
+    # Print the direction
     printf("\nDireção\n");
-    print_vec(bind, dB);
+    print_bind(bind, d);
 
     # Compute Theta*
-    #
-    [theta, aux] = thetaStep(xB, dB, m);
+    [theta, aux] = thetaStep(v(bind), d(bind), m);
+    printf("\nTheta*\n%f\n", theta);
 
     # Convert bind index to R^n index
+    # This index exits the base
     i = bind(aux);
-
-    printf("\nTheta*\n%f\n", theta);
     printf("\nSai da base: %d\n", i);
 
     # Compute new vector
-    vec = xB + theta*dB;
+    v += theta*d;
     ind = 1;
 endfunction
--- octave/src/simplex.m
+++ octave/src/simplex.m
@@ -1,39 +1,32 @@
 function [ind v] = simplex (A, b, c, m, n, x)
     printf("SIMPLEX: Fase 2\n");
 
+    # Vector starts with initial VBS
     v = x;
 
-    # Boolean vector, indicating if an index is basic or not
-    indexes = (v ~= 0);
     # Find basic and non-basic indexes
-    bind  = find(indexes);
-    nbind = find(~indexes);
+    bind  = find(v ~= 0);
+    nbind = find(v == 0);
 
     ind = 1;
     simplexstep = 0;
     while ind == 1;
-        # Print stuff
+        # Print iteration, basic variables and cost function
         printf("\n----------------------------------------\n")
         printf("\nIterando %d\n", simplexstep);
         print_bind(bind, v);
         printf("\nValor Função Objetivo: %f\n", c'*v);
 
         # Simplex iteration
-        [vec ind out in theta] = naiveStep(A, m, c, bind, nbind, v(bind));
+        [v ind out in] = naiveStep(A, m, n, c, bind, nbind, v);
 
-        # This is boring.
-        # We should compute d (both its basic and non-basic components)
-        # and then add Theta*d to x
-        # Also, beware when vec is a direction (cost = -Inf)
+        # Update basic and non-basic indexes, if necessary
         if ind == 1
-            v(bind) = vec;
-            v(nbind) = 0;
-            v(in)    = theta;
-
             bind(find(bind == out)) = in;
             nbind(find(nbind == in)) = out;
         endif
 
+        # Next step
         simplexstep++;
     endwhile
 
